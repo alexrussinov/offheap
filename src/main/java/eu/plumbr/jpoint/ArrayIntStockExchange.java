372ed8977c849e79cbeb789d554173e2ca7e2454
alex <avoronets@naumen.ru>
2015-09-14 17:32:01 +0300

    *refactoring

--- src/main/java/eu/plumbr/jpoint/ArrayIntStockExchange.java
+++ src/main/java/eu/plumbr/jpoint/ArrayIntStockExchange.java
@@ -5,55 +5,87 @@ package eu.plumbr.jpoint;
  */
 public class ArrayIntStockExchange implements StockExchange {
 
-    private  int[] orders = new int[TRADES_PER_DAY * 4];
-    private int pointer = 0;
-    private int  offset = 4;
+    private int recordsCount = 0;
+    private int[] buffer;
+    private final ArrayTrade flyweight = new ArrayTrade();
 
-    public void setOrders(int[] orders) {
-        this.orders = orders;
+    private ArrayTrade get(final int index) {
+        final int offset = index * flyweight.getObjectSize();
+        flyweight.setObjectOffset(offset);
+        return flyweight;
     }
 
-    public void setPointer(int pointer) {
-        this.pointer = pointer;
-    }
-
-    public void setOffset(int offset) {
-        this.offset = offset;
-    }
-
-    public int getPointer() {
-
-        return pointer;
-    }
-
-    public int getOffset() {
-        return offset;
-    }
-
-    public int[] getOrders() {
-
-        return orders;
+    public ArrayIntStockExchange() {
+        buffer = new int[TRADES_PER_DAY * flyweight.getObjectSize()];
     }
 
     @Override
     public void order(int ticket, int amount, int price, boolean buy) {
-        int[] orders = getOrders();
-        orders[pointer] = ticket;
-        orders[pointer + 1] = amount;
-        orders[pointer + 2] = price;
-        orders[pointer + 3] = buy ? 1 : 0;
-        setPointer(getPointer() + getOffset());
+        ArrayTrade trade = get(recordsCount++);
+        trade.setTicket(ticket);
+        trade.setAmount(amount);
+        trade.setPrice(price);
+        trade.setBuy(buy);
     }
 
     @Override
     public double dayBalance() {
-        int[] orders = getOrders();
-        int i = 0;
-        double result = 0.0;
-        while(i < TRADES_PER_DAY * offset - 5){
-            result = result + orders[i + 1] * orders[i + 2] * ((orders[i + 3] == 1) ? 1 : -1);
-            i += 5;
+        double balance = 0;
+        for (int i = 0; i < recordsCount; i++) {
+            ArrayTrade trade = get(i);
+            balance += trade.getAmount() * trade.getPrice() * (trade.isBuy() ? 1 : -1);
         }
-        return result;
+        return balance;
+    }
+
+    private class ArrayTrade {
+        private int offset = 0;
+
+        private final int ticketOffset = offset += 0;
+        private final int amountOffset = offset += 1;
+        private final int priceOffset = offset += 1;
+        private final int buyOffset = offset += 1;
+
+        private final int objectSize = offset += 1;
+
+        private int objectOffset;
+
+        public int getObjectSize() {
+            return objectSize;
+        }
+
+        void setObjectOffset(final int objectOffset) {
+
+            this.objectOffset = objectOffset;
+        }
+
+        public void setTicket(final int ticket) {
+            buffer[objectOffset + ticketOffset] = ticket;
+        }
+
+        public int getPrice() {
+            return buffer[objectOffset + priceOffset];
+        }
+
+        public void setPrice(final int price) {
+            buffer[objectOffset + priceOffset] = price;
+        }
+
+        public int getAmount() {
+
+            return buffer[objectOffset + amountOffset];
+        }
+
+        public void setAmount(final int quantity) {
+            buffer[objectOffset + amountOffset] = quantity;
+        }
+
+        public boolean isBuy() {
+            return buffer[objectOffset + buyOffset] == 1;
+        }
+
+        public void setBuy(final boolean buy) {
+            buffer[objectOffset + buyOffset]=  buy ? 1 : 0;
+        }
     }
 }
